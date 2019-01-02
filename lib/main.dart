ddb936374e20af5d346bb0b920ad4bec2aa49be8
Chris <christian.macias@cceo.com.mx>
2019-01-02 14:25:26 -0600

    stateless home

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,31 @@
 import 'package:first_flutter_app/ui/home.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(new Material(
+void main() => runApp(new MaterialApp(
     color: Colors.greenAccent,
- //   home: Home(),
-    child: new Center(
-      child: new Text("Hello world",
-      textDirection: TextDirection.ltr,
-      style: new TextStyle(fontWeight: FontWeight.w900, fontStyle: FontStyle.italic),
-      ),
-    ),
+    title: "Welcome app",
+    home: new Welcome(),
+
   ));
+
+class Welcome extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+    return new Material(
+      color: Colors.greenAccent,
+      child: new Center(
+        child: new Text(
+          "Welcome home",
+          textDirection: TextDirection.ltr,
+          style: new TextStyle(
+              fontWeight: FontWeight.w900,
+              fontStyle: FontStyle.italic,
+              fontSize: 35
+          ),
+        ),
+      )
+    );
+  }
+}
 
